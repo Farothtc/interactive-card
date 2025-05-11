927bd6963a384db6bf9c3fffe7f58c145ec76f81
Farothtc <mvkirimi@gmail.com>
2025-05-11 16:43:05 +0300

    Initial commit

---
+++ src/app/Components/SubmittedForm.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+
+export default function SubmittedForm({ onReset }: { onReset: () => void }) {
+  const handleSubmission = () => {
+    onReset();
+  };
+  return (
+    <Card className="w-[450px] h-auto border-0 shadow-none">
+      <CardContent className="flex items-center justify-center">
+        <img src="/icon-complete.svg" alt="Complete Icon" />
+      </CardContent>
+      <CardHeader className="text-center flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Thank you!</h1>
+        <p className="text-gray-500">We've added your card details</p>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center justify-center gap-4">
+        <Button
+          className="w-full cursor-pointer text-white"
+          onClick={handleSubmission}
+        >
+          Continue
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
